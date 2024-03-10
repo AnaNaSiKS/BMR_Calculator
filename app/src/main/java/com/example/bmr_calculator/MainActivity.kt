483f0bdc09415820f9f73627a8539c42ba063294
Влад Кантамиров <kantamirov2016@gmail.com>
2024-03-10 14:38:31 +0300

    add

--- app/src/main/java/com/example/bmr_calculator/MainActivity.kt
+++ app/src/main/java/com/example/bmr_calculator/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.bmr_calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var buttonCalculate: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonCalculate = findViewById(R.id.buttonCalculate)
+
+        buttonCalculate.setOnClickListener{
+            
+        }
     }
 }
